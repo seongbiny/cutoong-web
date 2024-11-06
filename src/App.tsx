78b5bd790e2df1846da874cb0c24ccd6409f9e5d
seongbiny <s30ngb1n@gmail.com>
2024-11-06 21:50:34 +0900

    🎉 project setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { Route, Routes } from "react-router";
-import Main from "./pages/Main";
+import "./App.css";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-    </Routes>
-  );
+  return <></>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,10 @@
-// import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
-// import GlobalStyle from "./GlobalStyle.ts";
-// import "./index.css";
-import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const GlobalStyle = createGlobalStyle`
-body {
-  max-width: 400px;
-  margin-left: auto;
-  margin-right: auto;
-  background-color: #f5f5f5;
-}
-`;
-
-const theme = {
-  pageBackground: "#fff",
-};
-
-createRoot(document.getElementById("root")!).render(
-  // <StrictMode>
-  <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>
-  // </StrictMode>
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
